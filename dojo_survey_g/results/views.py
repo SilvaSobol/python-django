369ea4f52630f197b435e8e8ce3bc1e18b24ea03
Silva Sobol <2017mspi@gmail.com>
2020-05-22 15:24:48 -0400

    dojo_survey up to date

--- dojo_survey_g/results/views.py
+++ dojo_survey_g/results/views.py
@@ -7,14 +7,14 @@ def display(request):
     if request.method == 'POST':
         context = {
             "name": request.POST["name"],
-            "loc": request.POST['location'],
-            "lan": request.POST['language'],
-            "com": request.POST['comments'],
-            # "gen": request.POST['gender'],
-            "how": request.POST['how']
+            "loc": request.POST["location"],
+            "lan": request.POST["language"],
+            "com": request.POST["comments"],
+            "gender": request.POST["gender"],
+            "how": request.POST["how"]
         }
-        return render(request,'output.html', context)
-    return render(request,'output.html')
+        # return render(request,'output.html', context)
+    return render(request,'output.html',context)
 
 
 
